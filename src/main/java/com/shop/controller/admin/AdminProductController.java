f7b4ec8ed9eb249747079d2151b6db78e885c650
trung thien <thien.nguyen@digiex.group>
2023-06-07 21:07:10 +0700

    jenkin new

--- src/main/java/com/shop/controller/admin/AdminProductController.java
+++ src/main/java/com/shop/controller/admin/AdminProductController.java
@@ -1,36 +1,106 @@
 package com.shop.controller.admin;
 
+import com.shop.entities.Product;
+import com.shop.repositories.CategoryRepository;
+import com.shop.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/admin/product")
 public class AdminProductController {
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("products", productRepository.findAll());
         return "admin/product/index";
     }
 
     @GetMapping("/create")
-    public String create() {
+    public String create(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+
+        model.addAttribute("product", new Product());
         return "admin/product/create";
     }
 
     @PostMapping("/create")
-    public String save() {
-        return "admin/product/create";
+    public String save(@Valid @ModelAttribute("product") Product product, BindingResult result, Model model, @RequestPart("file") MultipartFile file) {
+        if (result.hasErrors()) {
+            System.out.println(result);
+            model.addAttribute("categories", categoryRepository.findAll());
+            return "admin/product/create";
+        }
+        if (file.isEmpty()) {
+
+            model.addAttribute("error", "Vui lòng chọn một file để tải lên.");
+            return "admin/product/create";
+        }
+        try {
+            String originalFileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            Path filePath =  Paths.get("C:\\Users\\ACER\\OneDrive\\Desktop\\git_sport_shop\\src\\main\\resources\\static\\images", originalFileName);
+
+            Files.copy( file.getInputStream(),  filePath);
+
+            model.addAttribute("message", "Tải lên file thành công: " + originalFileName);
+            product.setImg(originalFileName);
+            productRepository.save(product);
+            return "redirect:/admin/product";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("error", "Lỗi xảy ra khi tải lên file.");
+            System.out.println(e.getMessage());
+            return "admin/product/create";
+        }
+
+
     }
 
-    @GetMapping("/edit")
-    public String edit() {
-        return "admin/product/edit";
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        Product product = productRepository.findById(id).orElse(null);
+
+        if (product != null) {
+            model.addAttribute("categories", categoryRepository.findAll());
+
+            model.addAttribute("product", product);
+            return "admin/product/edit";
+        } else {
+            return "not-found";
+
+        }
     }
 
 
     @PostMapping("/edit")
     public String update() {
         return "admin/product/edit";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        Product product = productRepository.findById(id).orElse(null);
+
+        assert product != null;
+        productRepository.delete(product);
+        return "redirect:/admin/product";
     }
 }
--- src/main/java/com/shop/entities/Category.java
+++ src/main/java/com/shop/entities/Category.java
@@ -1,6 +1,8 @@
 package com.shop.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -12,11 +14,12 @@ import java.util.List;
 @Data
 @Entity
 
-public class Category   {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @NotEmpty(message = "Tên thể loại không đượ trống")
+    @Size(max = 50, min = 1, message = "Tên thể loại không hợp lệ")
     private String name;
     @OneToMany(mappedBy = "category")
     @EqualsAndHashCode.Exclude
